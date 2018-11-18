d4515678f6fdc1f29016aa8cdfcfdf7d49a4c3e9
Tiago Luca <tledstiago@gmail.com>
2018-11-18 21:49:27 -0200

    Mod. inicio da lógica

--- algo.py
+++ algo.py
@@ -5,15 +5,27 @@ compv = []
 conv = []
 comp.append(1)
 compv.append(0)
-for i in range(0,(nc - 2)):
+for i in range(0, (nc - 2)):
     comp.append(int(input("Insira o número do computador:")))
     compv.append(int(input("Insira o valor do computador: ")))
-comp.append(50)
+comp.append(nc)
 compv.append(0)
-nc = int(input(print('Insira o número de conexões: \nSendo o computador do chef o de número 1 e o servidor o de número 50')))    
-for i in range(0,nc):
+nc = int(input(print('Insira o número de conexões: \nSendo o computador do chef o de número 1 e o servidor o �ltimo n�mero')))    
+for i in range(0, nc):
     con.append(input("Insira a conexão: (ComputadorX ComputadorY)"))
     conv.append(int(input("Insira o valor da conexão: ")))
+''' 
+Somando o valor das conex�es em �ndices diferentes
+'''
+dist = []
+x = 0
+for i in range(1, nc):
+    if comp[i - 1] + ' ' + comp[i] in con:
+        for j in range(1, nc):
+            if comp[i] + ' ' + comp[j] in con:
+                x += conv[j]
+        dist.append(x)
+        
 print(comp)
 print('\n')
 print(compv)
